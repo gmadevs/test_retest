20b5353ba1f17491ce7f11367e126ad31b7d5d05
Giorgio Maria Agazzi <giorgiomaria.agazzi@gmail.com>
2019-04-11 22:00:22 +0100

    First commit

---
+++ R/withinsd.R
@@ -0,0 +1,48 @@
+#' Within-subjects standard deviation
+#'
+#' Computation of within subjects standard deviation given two vectors of measurements
+#'
+#' @param x The vector containing the first set of measurements.
+#' @param y The vector containing the second set of measurements (must be the same lenght of x).
+#'
+#' @return The within-subject standard deviation according to QIBA.
+#'
+#' @author Giorgio Maria Agazzi, MD \email{giorgiomaria.agazzi@@gmail.com}
+#' @seealso Google Scholar \url{https://scholar.google.com/citations?user=JuDwBQsAAAAJ&hl=it}
+#' @keywords radiology repeatability
+#'
+#' @examples
+#' first <- rnorm(20, mean = 10, sd = 1)
+#' second <- rnorm(20, mean = 10, sd = 1)
+#'
+#' withinsd(first, second)
+#'
+#'
+#' @references 1. (2015) Quantitative Imaging Biomarkers: A Review of Statistical Methods for Technical Performance Assessment. Stat Methods Med Res 24:27–67. https://doi.org/10.1177/0962280214537344
+#'
+#' @export
+
+withinsd <- function(x, y) {
+  
+  #Add controlling if number of vectors is equal
+  N <- length(x)
+  Ny <- length(y)
+  
+  if( N!=Ny ) stop('The two vectors must have the same lenght')
+  
+  d <- NULL
+  for (i in 1:N) {
+    
+    d_temp <- (x[i] - y[i])^2
+    
+    d <- c(d, d_temp)
+    
+  }
+  
+  d <- sqrt(sum(d)/2*N)
+  
+  d <<- d
+  
+  cat("Within-subject variance is", d)
+  
+}
