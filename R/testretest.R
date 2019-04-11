d5097e60ade75c2b99f82386e15940daee3c76d2
Giorgio Maria Agazzi <giorgiomaria.agazzi@gmail.com>
2019-04-11 23:41:23 +0100

    Update testretest.R

--- R/testretest.R
+++ R/testretest.R
@@ -1,8 +1,6 @@
 #' Test retest: A package for simple test retest statistics in radiology
 #' 
 #' @section Package functions:
-#' 
-#' \code{\link[testretest]{withinsdlog}}
 #'
 #' @docType package
 #' @name testretest
